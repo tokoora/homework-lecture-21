c181a9168a5af9abe740e586eb2e28fd7d294702
Tornike Oragvelidze <toel@Tornikes-MacBook-Air.local>
2022-11-23 23:43:49 +0400

    Three Problems Done

---
+++ pointers-and-vectors/pointers.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    //მე-4 სავარჯიშო, აქედან დავიწყოთ დანარჩენი ცარიელი cout-ებია
+    int firstNumber, secondNumber, *firstPointer, *secondPointer, numberSum;
+    cout<<"Enter the first number: ";
+    cin>>firstNumber;
+    cout<<"Enter the second number: ";
+    cin>>secondNumber;
+    firstPointer=&firstNumber;
+    secondPointer=&secondNumber;
+    numberSum=*firstPointer+*secondPointer;
+    cout<<"The sum of numbers = "<<numberSum;
+    cout<<endl<<endl;
+
+    //მე-5 სავარჯიშო
+    int numberOne, numberTwo, sum;
+    cout<<"Enter the first number: ";
+    cin>>numberOne;
+    cout<<"Enter the second number: ";
+    cin>>numberTwo;
+    int &refOne=numberOne, &refTwo=numberTwo;
+    sum=refOne+refTwo;
+    cout<<"The sum of numbers = "<<sum;
+    cout<<endl<<endl;
+
+    //მე-6 სავარჯიშო
+    int numberSix, numberTwoSix, *pointerSix, *pointerTwoSix, maxSix;
+    cout<<"Enter the first number: ";
+    cin>>numberSix;
+    cout<<"Enter the second number: ";
+    cin>>numberTwoSix;
+    pointerSix=&numberSix, pointerTwoSix=&numberTwoSix;
+    if(*pointerSix>=*pointerTwoSix) maxSix=*pointerSix;
+    else maxSix=*pointerTwoSix;
+    cout<<"The max number is: "<<maxSix;
+    cout<<endl<<endl;
+
+    //მე-7 სავარჯიშო
+
+}
